d28f28f66925b5ca9629a472207d3f832b03776e
aruzhankuat <aruzhan.kuat@narxoz.kz>
2026-03-15 21:33:01 +0500

    4

--- src/com/narxoz/rpg/composite/EnemyUnit.java
+++ src/com/narxoz/rpg/composite/EnemyUnit.java
@@ -1,7 +1,7 @@
 package com.narxoz.rpg.composite;
 
 public class EnemyUnit extends UnitLeaf {
-    public EnemyUnit(String name, int health, int attackPower) {
-        super(name, health, attackPower);
+    public EnemyUnit(String name, int hp, int def) {
+        super(name, "Enemy", hp, 0, def);
     }
 }
