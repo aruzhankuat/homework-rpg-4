f68e063ba3d80075db6143d95583ba76a29b2f97
aruzhankuat <aruzhan.kuat@narxoz.kz>
2026-03-15 21:30:16 +0500

    4

--- src/com/narxoz/rpg/composite/CombatNode.java
+++ src/com/narxoz/rpg/composite/CombatNode.java
@@ -1,13 +1,8 @@
 package com.narxoz.rpg.composite;
 
-import java.util.List;
-
 public interface CombatNode {
-    String getName();
-    int getHealth();
-    int getAttackPower();
     void takeDamage(int amount);
+    void displayStatus(String indent);
+    String getName();
     boolean isAlive();
-    List<CombatNode> getChildren();
-    void printTree(String indent);
 }
