761b47e549fa944431432e66167e91be50a1b1df
aruzhankuat <aruzhan.kuat@narxoz.kz>
2026-03-15 23:22:27 +0500

    4

--- src/com/narxoz/rpg/bridge/SingleTargetSkill.java
+++ src/com/narxoz/rpg/bridge/SingleTargetSkill.java
@@ -1,16 +1,16 @@
 package com.narxoz.rpg.bridge;
-
 import com.narxoz.rpg.composite.CombatNode;
 
 public class SingleTargetSkill extends Skill {
-    public SingleTargetSkill(String skillName, int basePower, EffectImplementor effect) {
-        super(skillName, basePower, effect);
+    public SingleTargetSkill(String name, int mana, EffectImplementor imp) {
+        super(name, mana, imp);
     }
 
     @Override
-    public void cast(CombatNode target) {
-        // TODO: Single-target Bridge action
-        // 1) Resolve final damage through effect implementor
-        // 2) Apply to target node
+    public void use(CombatNode target) {
+        int damage = (int) (150 * implementor.getMultiplier());
+        System.out.println("Using SingleTargetSkill: " + skillName + " on " + target.getName());
+        implementor.apply(skillName, damage);
+        target.takeDamage(damage);
     }
 }
